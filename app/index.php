a23ccb06b2eefce2f7b54b7254ee6d89aac742c0
Kleber Machado <kleber.machado@ufvjm.edu.br>
2015-07-15 08:35:39 -0300

    Implementing initial authentication logic

--- app/index.php
+++ app/index.php
@@ -1,8 +1,11 @@
-<?php 
+<?php
     require_once 'smarty3/Smarty.class.php';
+    require_once dirname(__file__).'/../model/Ldap.php';
+
+    session_start();
         
     $s = new Smarty;
-    
+
     //Diretório de templates
     $s->addTemplateDir("../view/templates");
     
@@ -12,7 +15,20 @@
     //Criação de variável
     //$s->assign('titulo', 'Bem Vindo!');
 
-    //Display de página que utiliza template "template"
-    $s->display('index.html');
-    
-?>
+//    if (!array_key_exists('login', $_SESSION) && $_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_POST['login'] != null && $_POST['password'] != null) {
+        $ldap = new Ldap();
+        if ($ldap->autenticacao($_POST['login'], $_POST['password'])) {
+            echo 'usuario autenticado';
+            $_SESSION['login'] = $_POST['login'];
+            $s->display('info_siga.html');
+        } else {
+            $s->assign('erro', true);
+            $s->display('index.html');
+        }
+    } else {
+        echo 'sem valores para comparar';
+        unset($_SESSION);
+        //var_dump($_SESSION);
+        $s->display('index.html');
+    }
