6910c42984bb915da0959292596ffa3e1d9636a9
Kleber Machado <kleber.machado@ufvjm.edu.br>
2015-07-23 11:10:29 -0300

    Initial registration form solicitacao in LDAP

--- app/info_siga.php
+++ app/info_siga.php
@@ -1,6 +1,7 @@
 <?php 
     require_once 'smarty3/Smarty.class.php';
-    require_once dirname(__file__).'/../model/Siga.php';    
+    require_once dirname(__file__).'/../model/Siga.php';
+    require_once dirname(__file__).'/../model/Ldap.php';
 
     session_start();
     if (!array_key_exists('cpf', $_SESSION))
@@ -21,5 +22,24 @@
         $usuario = $siga->getUsuario($_SESSION['cpf']);
         $s->assign('usuario', $usuario);
     }
+    if (array_key_exists('login', $_POST) && $_POST['senha'] != "" && $_POST['check_senha'] != ""){
+        if ($_POST['senha'] == $_POST['check_senha']){
+            unset($_POST['check_senha']);
+            $ldap = new Ldap;
+            $usuarioLdap = new Usuario;
+
+            //Dados provisorios
+            $usuarioLdap->sobrenome = "teste";
+            $usuarioLdap->apelido = "teste";
+
+            foreach ($_POST as $key => $value){
+                $usuarioLdap->$key = $value;
+            }
+            if ($ldap->gravar($usuarioLdap)){
+                echo "passou";
+            }
+        }else{
+            $s->assign('error', true);
+        }
+    }
     $s->display('info_siga.html');
-?>
