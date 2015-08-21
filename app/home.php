85e34ca52de5d1576c75c9614018170e53d1acf7
pitterpereira <pitter.pereira@ufvjm.edu.br>
2015-08-21 14:21:55 -0300

    Modifications in the Profile's design

--- app/home.php
+++ app/home.php
@@ -1,40 +1,41 @@
-<?php 
-    require_once 'smarty3/Smarty.class.php';
-    require_once dirname(__file__).'/../model/Siga.php';
-    require_once dirname(__file__).'/../model/Ldap.php';
+<?php
 
-    session_start();
-    
-    if (!array_key_exists('login', $_SESSION)){
-        header('Location: index.php');
-        exit;
-    }
+require_once 'smarty3/Smarty.class.php';
+require_once dirname(__file__) . '/../model/Ldap.php';
 
-    $s = new Smarty;
+session_start();
 
-    //Diretório de templates
-    $s->addTemplateDir("../view/templates");
+//Se a variável login não existir na sessão, não deixar passar do index
+if (!array_key_exists('login', $_SESSION)) {
+    header('Location: index.php');
+    exit;
+}
+
+$s = new Smarty;
+
+//Diretório de templates
+$s->addTemplateDir("../view/templates");
+
+//Diretório de templates compilados
+$s->setCompileDir("../view/com_templates");
+
+//Display de página que utiliza template "template"
+$ldap = new Ldap;
+$usuario = $ldap->getUsuario($_SESSION['login']);
+
+//Verifica se o usuário possui foto
+if ($usuario->foto) {
+    //Se existir, baixar a foto para a pasta do site e associar valor verdadeiro à variável foto, usada no html da "home"
+    file_put_contents("imagens/" . $usuario->login . ".jpg", $usuario->foto);
     
-    //Diretório de templates compilados
-    $s->setCompileDir("../view/com_templates");
-    
-    //Display de página que utiliza template "template"
-    if (array_key_exists('login', $_SESSION)) {
-        $ldap = new Ldap;
-        $usuario = $ldap->getUsuario($_SESSION['login']);
-        
-        if ($usuario->foto) {
-            file_put_contents("imagens/" . $usuario->login . ".jpg", $usuario->foto);
-            
-            $s->assign('foto', true);
-        }
-        else{
-            $s->assign('foto', false);
-        }
-        
-        $s->assign('usuario', $usuario);
-        
-    }
-    
-    $s->display('home.html');
-    
+    //Este valor é utilizado num if. Se o usuário tiver foto, carregar a foto dele. Se não, carregar a imagem padrão
+    $s->assign('foto', true);
+} else {
+    $s->assign('foto', false);
+}
+
+//Associa à variável usuário, um objeto recheado de valores para serem utilizados no html da home
+$s->assign('usuario', $usuario);
+
+//Mostra a página home
+$s->display('home.html');
