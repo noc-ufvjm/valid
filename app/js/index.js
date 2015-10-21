d59994f830a0ebd81b52c01fbcecc933ad6b9111
Pitter Pereira <pitterpereira@gmail.com>
2015-10-21 11:28:06 -0200

    Index changes

--- app/js/index.js
+++ app/js/index.js
@@ -1,52 +1,49 @@
 function autenticacao(e) {
-    
+
     //Envia os dados para serem processados pela api getsiga.php
     $.post("/valid/app/api/getSiga.php", {
         cpf: $('#cpf').val(),
         senha: $('#senha').val()
     }, function (data, status) {
-        
+
         //Se o post tiver dado certo, verificar a resposta
         if (status === 'success') {
-            
+
             //Existe no siga e não no ldap
             if (data == 'true') {
-                
+
                 //Executar o submit do form
                 $("#form_solicitacao").submit();
-                
-              //Se o usuário já existir dentro do ldap, ele já possui acesso direto ao valid, então, mostra a mensagem
+
+                //Se o usuário já existir dentro do ldap, ele já possui acesso direto ao valid, então, mostra a mensagem
             } else if (data == 'Already exists') {
                 
-                //Seta esta mensagem ao div
-                $("#msg_error p").text("Usuário já possui acesso ao ValID!");
-                
-                //Mostra a mensagem
-                $("#msg_error").show("slow");
-                
-              //Erro no cpf ou na senha
+                mensagem("Usuário já possui acesso ao ValID!");
+
+                //Se o usuário já tiver realizado a solicitação de acesso
             } else if (data == 'Already in progress') {
-                
-                //Seta esta mensagem ao div
-                $("#msg_error p").text("Usuário já solicitou acesso ao ValID!");
-                
-                //Mostra a mensagem
-                $("#msg_error").show("slow");
-                
-              //Erro no cpf ou na senha
+
+                mensagem("Usuário já solicitou acesso ao ValID!");
+
+                //Erro no cpf ou na senha
             } else {
-                $("#msg_error p").text("Favor verificar CPF e/ou senha!");
-                $("#msg_error").show("slow");
+                mensagem("Favor verificar CPF e/ou senha!");
             }
         }
     });
-    
+
     //Evita que o botão execute sua ação default, executando as definidas pelo javascript
     e.preventDefault();
 }
 
+//Mensagem que aparece de acordo com a ocasião
+function mensagem(texto) {
+    $("#msg_error p").text(texto).addClass('alert-danger').addClass('alert');
+    $("#msg_error").show("slow");
+}
+
 $(document).ready(function () {
-    
+
     //Ao se preencher o cpf e a senha do siga, o envio da solicitação de acesso é feito
     $("#solicitar").click(autenticacao);
 
